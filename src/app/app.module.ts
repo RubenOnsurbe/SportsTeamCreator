52c161b774313a0615f6dec8a1585f80500469c7
AlejandroGarJ <alejgarciaj@gmail.com>
2024-03-11 22:47:07 +0100

    Landing Page V1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,34 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './contacto/contacto.component';
+import { HomeComponent } from './home/home.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+const appRoutes:Routes=[
+
+  {path:'',component:HomeComponent},
+  {path:'contacto',component:ContactoComponent}
+  
+ 
+
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+    FontAwesomeModule
   ],
   providers: [
     provideClientHydration()
